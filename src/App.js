e819eb3edd6fd5785e92a196209744ebcf71b0da
Kauk777 <curokky777@gmail.com>
2020-11-28 09:00:41 +0530

    [KAUSHAL]Added: React Intro Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import StudentApp from './studentList.jsx';
+import React from 'react';
+import logo from './assets/BL_logo_square_jpg.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  url="http://bridgelabz.com/";
+  constructor() {
+    super()
+    this.state= {
+      userName: '',
+      nameError: ''
+    }
+  }
+  onClick = ($event) => {
+    console.log("save button is clicked! ", $event);
+    window.open(this.url,"_blank");
+  }
+  onNameChange = (event) => {
+    console.log("Value is: ",event.target.value);
+    this.setState({userName:event.target.value});
+    let nameReg=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameReg.test(event.target.value))
+    this.setState({nameError: ''});
+    else
+    this.setState({nameError: 'Invalid Name!'});
+  }
+
+  render() {
+    return (
+      <>
+      <div>
+        <h2>Hello {this.state.userName} from the other world!!</h2>
+        <img src={logo} onClick={this.onClick} alt="sample" />
+      </div>
+      <div>
+        <input onChange={this.onNameChange} />
+        <span className="error-output">{this.state.nameError}</span>
+      </div>
+      </>
+      /*<div>
+        <StudentApp />
+      </div>*/
+    );
+  }
 }
-
 export default App;
